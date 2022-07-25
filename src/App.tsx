966ffe8c61bbbca8ea4e89719bd7d2e1bf3e4df3
Edward Peña <58954908+edwardp22@users.noreply.github.com>
2022-07-25 10:35:38 -0700

    Layout working with main container

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,16 @@ import React from "react";
 
 import { Routes, Route } from "react-router-dom";
 
+import { Layout } from "./components";
 import { ShowingNow } from "./pages";
 
 function App() {
   return (
-    <div>
+    <Layout>
       <Routes>
         <Route path="/" element={<ShowingNow />} />
       </Routes>
-    </div>
+    </Layout>
   );
 }
 
--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,5 +1,16 @@
 import React, { ReactElement } from "react";
+import { Col, Container, Row } from "react-bootstrap";
 
 export default function Layout({ children }: { children: ReactElement }) {
-  return <div>{children}</div>;
+  return (
+    <Container fluid id="mainContainer">
+      <Row>
+        <Col xs={12} md={{ span: 10, offset: 1 }}>
+          {/* Header */}
+          {/* Menu */}
+          {children}
+        </Col>
+      </Row>
+    </Container>
+  );
 }
