89b83a5f141b01bd5a2e149a33d31e1ae665a94e
Edward Peña <58954908+edwardp22@users.noreply.github.com>
2022-07-27 08:58:28 -0700

    Generating the menu

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,24 @@ import { Routes, Route } from "react-router-dom";
 import { Layout } from "./components";
 import { ShowingNow } from "./pages";
 
+export const menu = [
+  { route: "/", text: "Showing Now", element: <ShowingNow /> },
+  { route: "/comming", text: "Coming soon" },
+  { route: "/top", text: "Top 10" },
+  { route: "/popular", text: "Most Popular" },
+  { route: "/favorites", text: "Favorites" },
+  { route: "/about", text: "About Us" },
+  { route: "/contact", text: "Contact Us" },
+  { route: "/register", text: "Register Now" },
+];
+
 function App() {
   return (
     <Layout>
       <Routes>
-        <Route path="/" element={<ShowingNow />} />
+        {menu.map((link) => (
+          <Route path={link.route} element={link.element} />
+        ))}
       </Routes>
     </Layout>
   );
--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -1,5 +1,19 @@
 import React from "react";
 
+import { Nav } from "react-bootstrap";
+import { menu } from "../../App";
+
 export default function Menu() {
-  return <div>Menu</div>;
+  return (
+    <Nav
+      activeKey="/home"
+      onSelect={(selectedKey) => alert(`selected ${selectedKey}`)}
+    >
+      {menu.map((link) => (
+        <Nav.Item>
+          <Nav.Link href={link.route}>{link.text}</Nav.Link>
+        </Nav.Item>
+      ))}
+    </Nav>
+  );
 }
